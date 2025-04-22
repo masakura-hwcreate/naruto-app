4f2ccba39ae8e0cc6da75cb67172c0b3eee0ea8a
masakura-hwcreate <masakura@hw-create.com>
2025-04-22 22:35:59 +0900

    fetch_image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
   useEffect(()=> {
     fetchCharacters();
   }, []);
@@ -11,10 +13,20 @@ function App() {
     const apiUrl = 'http://localhost:80/character';
 
     const result = await axios.get(apiUrl);
+    setCharacters(result.data.characters);
     console.log(result);
   };
   return (
-    <div className="App">hello world
+    <div className="container">
+      <main>
+        <div  className='cards-container'>
+          {characters.map((character) => {
+            return <div className='card' key={character.id}>
+              <img src={character.images[0] != null ? character.images[0] : 'dummy.png'} alt="character" className='card-image'  />
+            </div>
+          })}
+        </div>
+      </main>
     </div>
   );
 }
